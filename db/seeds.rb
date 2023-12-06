67ed64540c6d9bb0c0e2a31f0d904a8a37d27025
stwardak <148924249+stwardak@users.noreply.github.com>
2023-12-06 13:06:56 -0600

    added model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+song = Song.new(
+  title: "Thank You",
+  album: "Led Zeppelin II",
+  artist: "Led Zeppelin",
+  year: 1969,
+)
+song.save
+song = Song.new(
+  title: "Halah",
+  album: "She Hangs Brightly",
+  artist: "Mazzy Star",
+  year: 1990,
+)
+song.save
+song = Song.new(
+  title: "Praise You",
+  album: "You've Come a Long Way, Baby",
+  artist: "Fatboy Slim",
+  year: 1998,
+)
+song.save
+song = Song.new(
+  title: "Shiny Happy People",
+  album: "Out of Time",
+  artist: "REM",
+  year: 1991,
+)
+song.save
